ba5b80e1a9b0eadf65b515cb66fe8527c1ed8f9c
Zeeshan Nazir <zeeshannazir042@gmail.com>
2025-12-07 16:06:59 +0100

    some changes 2

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./i18n";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/jobperhour">
       <App />
     </BrowserRouter>
   </React.StrictMode>
