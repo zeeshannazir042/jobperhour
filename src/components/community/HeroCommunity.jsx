9337a03cbc73ed67a7641841fc557f1b7bb87eb4
Zeeshan Nazir <zeeshannazir042@gmail.com>
2025-12-31 00:09:58 +0100

    Update website community image

--- src/components/community/HeroCommunity.jsx
+++ src/components/community/HeroCommunity.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import communityImg from "../../assets/Images/community/herocommunity.jpg";
+import communityImg from "../../assets/Images/community/community-hero.jpeg";
 
 const HeroCommunity = () => {
   return (
